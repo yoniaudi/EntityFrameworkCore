1c4616ce629988993f75a7ed9013d24b6a45a92d
Yoni Audi <yoniaudi@gmail.com>
2024-09-03 11:50:05 +0300

    Init commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
