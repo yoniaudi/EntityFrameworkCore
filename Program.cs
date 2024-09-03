c0520655a73264bbf9ff37e575572b2415a7c99c
Yoni Audi <yoniaudi@gmail.com>
2024-09-03 15:13:48 +0300

    Add EF Core to minimal API

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -23,5 +25,36 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
+{
+    await db.Pizzas.AddAsync(pizza);
+    await db.SaveChangesAsync();
 
+    return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+app.MapPut("/pizza/{id}", async(PizzaDb db, Pizza updatePizza, int id) =>
+{
+    Pizza? pizza = await db.Pizzas.FindAsync(id);
+    IResult connectionResult = Results.NotFound();
+
+    if (pizza != null)
+    {
+        pizza.Name = updatePizza.Name;
+        pizza.Description = updatePizza.Description;
+        await db.SaveChangesAsync();
+        connectionResult = Results.NoContent();
+    }
+
+    return connectionResult;
+});
+app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza == null) return Results.NotFound();
+    db.Pizzas.Remove(pizza);
+    await db.SaveChangesAsync();
+    return Results.Ok();
+});
 app.Run();
